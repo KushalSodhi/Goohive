1738962143f70c214e01d97318218e27ea745a3b
Kushal <kushalsodhi25@gmail.com>
2026-04-28 14:19:21 +0530

    Added backend

---
+++ Backend/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, send_from_directory
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+files = []
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+    if file.filename not in files:
+        files.append(file.filename)
+    return "Uploaded"
+
+@app.route('/files')
+def get_files():
+    return {"files": files}
+
+@app.route('/delete/<filename>')
+def delete(filename):
+    path = os.path.join(UPLOAD_FOLDER, filename)
+    if os.path.exists(path):
+        os.remove(path)
+    if filename in files:
+        files.remove(filename)
+    return "Deleted"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
